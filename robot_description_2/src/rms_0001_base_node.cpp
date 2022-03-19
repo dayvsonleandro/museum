9a0d87157247fffb79f7858bd07a20dafdfed721
dayvsonleandro <dayvsonleandro@gmail.com>
2022-03-19 12:16:22 -0300

    remove robot_description

--- robot_description_2/src/rms_0001_base_node.cpp
+++
@@ -1,9 +0,0 @@
-// #include "rms_0001_base.h"
-
-// int main(int argc, char **argv)
-// {
-//     ros::init(argc, argv, "rms_0001_base_node");
-//     RmsBase rms;
-//     ros::spin();
-//     return 0;
-// }
